8f0a801f4c97c973e2eb25f1287c47b771b5d8ed
TanmayKothari-stack <kotharijitendra960@gmail.com>
2025-11-30 09:46:35 +0000

    Workflow

--- todo-list/capacitor.config.ts
+++ todo-list/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.app.todolist',
-  appName: 'todo-list',
+  appName: 'Todo List',
   webDir: 'out',
 };
 
